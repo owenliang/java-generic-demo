660c380c56d66d5d2cfc3701e13f923ec69717a4
owenliang <owenliang1990@gmail.com>
2020-11-08 13:51:35 +0800

    update

--- src/main/java/cc/yuerblog/Main.java
+++ src/main/java/cc/yuerblog/Main.java
@@ -6,44 +6,69 @@ public class Main {
     // 多态1
     private static void demo1() {
         Student a = new Student();
+
+        // 正确
         Person b = a;
     }
     // 多态2
     private static void demo2() {
         Student[] a = new Student[5];
+
+        // 正确
         Person[] b = a;
     }
     // 泛型1
     private static void demo3() {
         List<Student> a = null;
-        List<Person> b = a; // 报错，List<A>和List<B>不受A,B多态影响
+        List<Person> b = a; // 错误：List<Student>和List<Person>是两个不同的类型，不受Student与Person本身多态影响
     }
     // 泛型2
     private static void demo4() {
         List<Student> a = null;
         List<?> b = a;
-        b.add(new Student()); // 报错，我们并不知道?是什么类型，所以不能添加任何元素
 
-        Object o = b.get(0);    // 无论?是什么类型，都是Object的子类
+        // 错误：编译器不知道?原本是什么类型，所以添加任何元素都是禁止的
+        b.add(new Student());
+        b.add(new Person());
+        b.add(new Object());
+
+        // 正确，无论?原本是什么类型，Object都是基类，符合多态向上转换
+        Object o = b.get(0);
     }
     // 泛型3
     private static void demo5() {
         List<Student> a = null;
-        List<? extends Person> b = a;   // ?通配Person的所有子类
-        b.add(new Student()); // 报错，我们并不知道?到底是Person的哪个子类
-        b.add(new Person());    // 报错，?是Person的某个子类，我们并不知道具体是哪种类型
 
-        Person o = b.get(0);    // 无论?是什么类型，都是Person的子类
+        // 正确：?通配任意的Person子类
+        List<? extends Person> b = a;
+
+        // 报错：虽然?是Person的某个子类，但是我们并不知道List中的真实数据类型，因此禁止添加元素
+        b.add(new Student());
+        b.add(new Person());
+
+        // 正确：无论?是Person的哪个子类，它们都以Person为基类
+        Person o = b.get(0);
     }
     // 泛型4
     private static void demo6() {
         List<Person> a = null;
-        List<? super Student> b = a;    // ?通配Student的所有父类
 
-        b.add(new Student());   // ?一定是Student的父类，所以add Student符合多态
-        b.add(new ClassLeader());   // ?一定是Student的父类，也一定是ClassLeader的父类（因为ClassLeader继承Student），符合多态
-        b.add(new Person());    // 报错，?虽然是Student的父类，但并不知道具体是哪个父类，所以无法add父类对象
+        // 正确：?通配Student的任意父类
+        List<? super Student> b = a;
+
+        // 正确：?通配任意Student父类，因此Student和ClassLeader都符合多态向上转换
+        b.add(new Student());
+        b.add(new ClassLeader());
+
+        // 错误：?通配Student父类，但Student父类不一定只有Person，所以<? super>没法add父类的实例
+        b.add(new Person());
+
+        // 正确：?通配Student任意父类，但是?也不知道具体是哪个，所以只能多态向上为Object
+        Object o = b.get(0);
     }
     public static void main(String[] args) {
+        // 总结一下:
+        // 1,<? extends A> 通配A的子类，禁止任意add，可以get为A类
+        // 2,<? super A> 通配A的父类，可以add任意A的子类对象，可以get为Object
     }
 }
